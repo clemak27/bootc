32b98f40f4c1963fb473fb6501e144dcd8b73e7a
clemak27 <clemak27@mailbox.org>
2026-04-08 18:10:28 +0200

    fix(plasma): use packages from copr instead of building

--- build/02_plasma.sh
+++ build/02_plasma.sh
@@ -4,39 +4,27 @@ set -ouex pipefail
 
 dnf -y install git cmake extra-cmake-modules gettext
 
-# klassy
+## packages
 
-# renovate: datasource=github-tags depName=paulmcauley/klassy versioning=loose
-klassy_version=v6.5.3
+dnf -y copr enable scottames/ghostty
+dnf -y copr enable ama1470/kwin-effects-glass
+dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo
 
-cd /tmp
-dnf -y install "cmake(KF5Config)" "cmake(KF5CoreAddons)" "cmake(KF5FrameworkIntegration)" "cmake(KF5GuiAddons)" "cmake(KF5Kirigami2)" "cmake(KF5WindowSystem)" "cmake(KF5I18n)" "cmake(Qt5DBus)" "cmake(Qt5Quick)" "cmake(Qt5Widgets)" "cmake(Qt5X11Extras)" "cmake(KDecoration3)" "cmake(KF6ColorScheme)" "cmake(KF6Config)" "cmake(KF6CoreAddons)" "cmake(KF6FrameworkIntegration)" "cmake(KF6GuiAddons)" "cmake(KF6I18n)" "cmake(KF6KCMUtils)" "cmake(KF6KirigamiPlatform)" "cmake(KF6WindowSystem)" "cmake(Qt6Core)" "cmake(Qt6DBus)" "cmake(Qt6Quick)" "cmake(Qt6Svg)" "cmake(Qt6Widgets)" "cmake(Qt6Xml)"
-git clone https://github.com/paulmcauley/klassy --branch=$klassy_version
-cd klassy
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
-make --jobs "$(nproc)"
-make install
+dnf -y install \
+  adw-gtk3-theme \
+  ghostty \
+  klassy \
+  kwin-effects-glass \
+  papirus-icon-theme
 
-# kwin-effects-glass
+dnf -y remove \
+  konsole
 
-# renovate: datasource=github-tags depName=4v3ngR/kwin-effects-glass versioning=loose
-kwin_effects_glass_version=6.6.3-1
-cd /tmp
-dnf -y install kf6-kglobalaccel kf6-kdeclarative libplasma kf6-kio qt6-qtbase kf6-kguiaddons kf6-ki18n rpm-build
-dnf -y install wayland-devel libdrm-devel kf6-kwindowsystem-devel plasma-workspace-devel libplasma-devel qt6-qtbase-private-devel qt6-qtbase-devel kwin-devel kwin-devel kf6-knotifications-devel kf6-kio-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kguiaddons-devel libepoxy-devel kf6-kglobalaccel-devel kf6-kcmutils-devel kf6-kconfigwidgets-devel kf6-kdeclarative-devel kdecoration-devel wayland-devel libdrm-devel
-git clone https://github.com/4v3ngR/kwin-effects-glass --branch=$kwin_effects_glass_version
-cd kwin-effects-glass
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
-make --jobs "$(nproc)"
-cpack -V -G RPM
-dnf install -y /tmp/kwin-effects-glass/build/kwin-glass.rpm
-rm -rf /tmp/kwin-effects-glass
+rm -f /etc/yum.repos.d/home:paulmcauley.repo
+dnf -y copr disable ama1470/kwin-effects-glass
+dnf -y copr disable scottames/ghostty
 
-dnf -y remove wayland-devel libdrm-devel kf6-kwindowsystem-devel plasma-workspace-devel libplasma-devel qt6-qtbase-private-devel qt6-qtbase-devel kwin-devel kwin-devel kf6-knotifications-devel kf6-kio-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kguiaddons-devel libepoxy-devel kf6-kglobalaccel-devel kf6-kcmutils-devel kf6-kconfigwidgets-devel kf6-kdeclarative-devel kdecoration-devel wayland-devel libdrm-devel
+## plasmoids
 
 # renovate: datasource=github-tags depName=peterfajdiga/karousel versioning=loose
 karousel_version=0.16
@@ -82,17 +70,3 @@ curl -fL -o /tmp/kgc.tar.gz https://github.com/peterfajdiga/kwin4_effect_geometr
 tar xzf /tmp/kgc.tar.gz
 mkdir -p "/usr/share/kwin/effects"
 mv /tmp/kwin4_effect_geometry_change "/usr/share/kwin/effects/kwin4_effect_geometry_change"
-
-# packages
-
-dnf -y copr enable scottames/ghostty
-
-dnf -y install \
-  adw-gtk3-theme \
-  ghostty \
-  papirus-icon-theme
-
-dnf -y remove \
-  konsole
-
-dnf -y copr disable scottames/ghostty
